617c02d748f782376aa34519cbb639b485fab20b
Matthew <matthew.l.gong@gmail.com>
2017-04-07 12:00:21 -0600

    Everything but the stupid FFT deconvolve funtion is up and running and I can't find the bug in the deconvolve

--- deblurring.py
+++ deblurring.py
@@ -30,7 +30,7 @@ def motion_psf(size, theta):
 
 	half_n = int(np.floor(n/2))	 	
 
-	kernel = np.zeros((n,n))
+	kernel = np.zeros((n,n)).astype('float32')
 
 	# fills in a horizontal line across the middle
 	kernel[half_n,:] = 1
@@ -66,19 +66,106 @@ def convolve(image, kernel):
 
 	# Save the spatial dimensions of the image and the kernel
 	image_height, image_width   = image.shape
-	kernel_height, kernel_width = kenrel.shape
+	kernel_height, kernel_width = kernel.shape
 
 	# pad the image 
 	pad = (kernel_width -1) /2
+	
+	proto_convolve = cv2.copyMakeBorder(image, pad, pad, pad, pad,
+										    cv2.BORDER_REPLICATE)
 
-	proto_convolve = cv2.copyMakeBorder(image, pad, pad, pad, pad)
+	convolved_image = np.zeros((image_height, image_width), dtype = "float32")
 
-	convovlve_imge = np.zeros((image_height, image_width), dtype = "float32")
+	# loop over the proto convolution, while sliding our kernel
+	# We're going left to right then top to bottom
+
+
+	# Top to bottom
+	for y in xrange(pad, image_height + pad):
+
+		# Left to right
+		for x in range(pad, image_width + pad):
+
+			# extract a region of interest(roi) to convolve
+			roi = proto_convolve[ y-pad:y+pad+1, x-pad:x+pad+1]
+
+			# perform the convolution using the kernel
+			# Element wise multiply and then sum the submatrix
+			conv_val = (roi * kernel).sum()
+		
+			# store the value in the output in the convolved image output
+			convolved_image[y-pad,x-pad] = conv_val
+		
+
+	# lastly we must rescale the image to be values between 0-255
+	convolved_image = convolved_image / np.amax(convolved_image)
+	convolved_image =(convolved_image * 255).astype('float32')
+
+
+	return convolved_image
+
+def deconvolve(convolved_image, PSF, epsilon = 1e-4):
+	"""
+	Inputs:
+
+		convolved_image:
+					PSF:
+				epsilon:
+
+
+	Outputs:
+
+		deconvolved_image:
 
 	"""
-	TO BE BUILT:
+	height, width = convolved_image.shape 
 
-	NESTED FOR LOOPS FOR MOVING CONVOLUTION
+	# in order for us to center the kernel we must have an array with odd dimensions
+	
+	# assume the that the array is odd if not set the padding to 1
+	h_pad = 0 if height%2 == 1 else 1
+	w_pad = 0 if  width%2 == 1 else 1
 
-	RETURN STATEMENTS 
-	"""
+	# pad on top and bottom
+	padded_convolve = cv2.copyMakeBorder(convolved_image,h_pad,0,w_pad,0, cv2.BORDER_REPLICATE)
+
+	# use the FFT to ppn the convolved image	
+	imag_fft = np.fft.fftn(padded_convolve)
+
+	# create the full sized array with the kernel centered in it
+	#	create the zeros array
+	new_kern = np.zeros_like(padded_convolve)
+
+	# center the kernel in the array
+	h_shift = (height - PSF.shape[0])/2 
+	w_shift = (width  - PSF.shape[1])/2 
+
+	new_kern[h_shift:h_shift+PSF.shape[0], w_shift:w_shift+PSF.shape[1]] += PSF/PSF.sum() + epsilon
+
+	plt.imshow(new_kern)
+	plt.show()
+
+	kern_fft = np.fft.fftn(new_kern)
+	print kern_fft,imag_fft
+	deconvol = np.divide(imag_fft,kern_fft)
+
+	deconvolved_image = np.abs(np.fft.ifftn(deconvol))
+	
+	return deconvolved_image	
+
+
+
+path = 'index.jpeg'
+image = cv2.imread(path, 0)
+#plt.imshow(image, cmap = 'gray')
+#plt.show()
+
+kernel = motion_psf(20,0)
+
+test_conv = convolve(image,kernel)
+#plt.imshow(test_conv, cmap='gray')
+#plt.show()
+
+restored = deconvolve(test_conv,kernel)
+plt.imshow(restored.reshape(restored.shape[0],restored.shape[1]), cmap = 'gray')
+plt.show()
