6b18f29969a07fcf1b999e9da54719a2983e85da
Matthew Gong <Matthew.l.gong@gmail.com>
2017-04-12 00:28:56 -0600

    I got deconvolution to work for motion PSFs but it's having trouble with focus PSFs still

--- deblurring.py
+++ deblurring.py
@@ -1,8 +1,10 @@
 import cv2
-
 import numpy as np
 
 from matplotlib import pyplot as plt
+import scipy.signal as signal
+
+from scipy import fftpack
 
 
 def motion_psf(size, theta):
@@ -29,7 +31,7 @@ def motion_psf(size, theta):
 	kernel = np.zeros((n,n)).astype('float32')
 
 	# fills in a horizontal line across the middle
-	kernel[half_n,:] = 1
+	kernel[half_n,half_n:] = 1
 
 	# rotate the kernel to the desired angle
 	center = (half_n ,half_n)
@@ -38,12 +40,30 @@ def motion_psf(size, theta):
 
 	kernel = cv2.warpAffine(kernel, rotation, (n,n))
 
-	"""
-	this was to see what the kernel looked like, might have a use for this still
-	plt.imshow(kernel, cmap = 'gray')
-	plt.show()
-	"""
+	
+	#this was to see what the kernel looked like, might have a use for this still
+	#plt.imshow(kernel, cmap = 'gray')
+	#plt.show()
+	
+	print np.sum(kernel)
 	return kernel
+
+def foucs_psf(l=5, sig=1.):
+    """
+    creates gaussian kernel with side length l and a sigma of sig
+    which approximates out of focus blur
+    """
+
+    n = l if (l%2 == 1) else l + 1 
+
+
+    ax = np.arange(-n // 2 + 1., n // 2 + 1.)
+    xx, yy = np.meshgrid(ax, ax)
+
+    kernel = np.exp(-(xx**2 + yy**2) / (2. * sig**2))
+
+    print np.sum(kernel)
+    return kernel
 
 def convolve(image, kernel):
 	"""
@@ -69,12 +89,11 @@ def convolve(image, kernel):
 	
 	proto_convolve = cv2.copyMakeBorder(image, pad, pad, pad, pad,
 										    cv2.BORDER_REPLICATE)
-
-	convolved_image = np.zeros((image_height, image_width), dtype = "float32")
+	
+	convolved_image = np.zeros_like(proto_convolve, dtype = "float32")
 
 	# loop over the proto convolution, while sliding our kernel
 	# We're going left to right then top to bottom
-
 
 	# Top to bottom
 	for y in xrange(pad, image_height + pad):
@@ -90,78 +109,162 @@ def convolve(image, kernel):
 			conv_val = (roi * kernel).sum()
 		
 			# store the value in the output in the convolved image output
-			convolved_image[y-pad,x-pad] = conv_val
+			convolved_image[y,x] = conv_val
 		
 
 	# lastly we must rescale the image to be values between 0-255
 	convolved_image = convolved_image / np.amax(convolved_image)
 	convolved_image =(convolved_image * 255).astype('float32')
 
+	image_to_return = convolved_image[pad:image_height+pad, pad:image_width+pad]
 
-	return convolved_image
+	return image_to_return
 
-def deconvolve(convolved_image, PSF, epsilon = 1e-4):
+def deconvolve(convolved_image, PSF):
 	"""
 	Inputs:
 
 		convolved_image:
 					PSF:
 				epsilon:
-
-
 	Outputs:
 
 		deconvolved_image:
 
 	"""
-	height, width = convolved_image.shape 
 
-	# in order for us to center the kernel we must have an array with odd dimensions
-	
-	# assume the that the array is odd if not set the padding to 1
-	h_pad = 0 if height%2 == 1 else 1
-	w_pad = 0 if  width%2 == 1 else 1
-
-	# pad on top and bottom
-	padded_convolve = cv2.copyMakeBorder(convolved_image,h_pad,0,w_pad,0, cv2.BORDER_REPLICATE)
-
-	# use the FFT to ppn the convolved image	
-	imag_fft = np.fft.fftn(padded_convolve)
-
-	# create the full sized array with the kernel centered in it
+	# create the full sized array with the kernel in it
 	#	create the zeros array
-	new_kern = np.zeros_like(padded_convolve)
+	new_kern = np.zeros_like(convolved_image)
 
-	# center the kernel in the array
-	h_shift = (height - PSF.shape[0])/2 
-	w_shift = (width  - PSF.shape[1])/2 
+	# add in the kernel
+	new_kern[:PSF.shape[0], :PSF.shape[1]] = PSF
 
-	new_kern[h_shift:h_shift+PSF.shape[0], w_shift:w_shift+PSF.shape[1]] += PSF/PSF.sum() + epsilon
-
-	plt.imshow(new_kern)
-	plt.show()
-
+	# use the FFT on the convolved image and kernel	
+	imag_fft = np.fft.fftn(convolved_image)
 	kern_fft = np.fft.fftn(new_kern)
-	print kern_fft,imag_fft
-	deconvol = np.divide(imag_fft,kern_fft)
-
-	deconvolved_image = np.abs(np.fft.ifftn(deconvol))
 	
-	return deconvolved_image	
+	# once in the image is in the FFT we can obtain the formula latent_image = blurred_image / PSF
+	# to obtain our deblurred image
+	deconvolved_image = np.abs(np.fft.ifftn(imag_fft/kern_fft))
+	
+	# the image will have shifted during the FFT so we shift it accordingly
+	deconvolved_image = np.roll(deconvolved_image, PSF.shape[0]/2, axis = 0)
+	deconvolved_image = np.roll(deconvolved_image, PSF.shape[1], axis = 1)
 
+	return deconvolved_image
 
-
+#load in the image and display it
 path = 'index.jpeg'
 image = cv2.imread(path, 0)
 plt.imshow(image, cmap = 'gray')
 plt.show()
 
-kernel = motion_psf(20,0)
+# generate our motion kernels
+motion_kernel = motion_psf(20,0)
+focus_kernel  = foucs_psf(11,5)
 
-test_conv = convolve(image,kernel)
+# show what our kernels look like
+plt.imshow(focus_kernel)
+plt.show()
+
+plt.imshow(motion_kernel)
+plt.show()
+
+# show what our image looks like convolved with the motion kernel
+test_conv  = convolve(image,motion_kernel)
 plt.imshow(test_conv, cmap='gray')
 plt.show()
 
-restored = deconvolve(test_conv,kernel)
-plt.imshow(restored.reshape(restored.shape[0],restored.shape[1]), cmap = 'gray')
+# show what our image looks like convolved with the focus kernel
+deacon = convolve(image, focus_kernel)
+plt.imshow(deacon, cmap = 'gray')
 plt.show()
+
+
+# show what our image looks like when it's deconvolved with the motion kernel
+original2  = np.real(deconvolve(test_conv, motion_kernel))
+plt.imshow(original2, cmap ='gray')
+plt.show()
+
+# show what our image looks like when it's deconvolved with the focus kernel
+# this is broken and I don't know why
+focus_blur = np.real(deconvolve(deacon, focus_kernel))
+plt.imshow(focus_blur, cmap ='gray')
+plt.show()
+
+new_kern = np.zeros_like(image)
+
+print focus_kernel
+
+new_kern[:focus_kernel.shape[0], :focus_kernel.shape[1]] = focus_kernel	
+# this is also broken and I don't know why
+print new_kern[:focus_kernel.shape[0], :focus_kernel.shape[1]].shape
+plt.imshow(new_kern)
+plt.show()
+
+def convolve2(star, psf):
+    star_fft = np.fft.fftn(star)
+    psf_fft = np.fft.fftn(psf)
+    return np.fft.ifftn(star_fft*psf_fft)
+
+def deconvolve2(star, psf):
+    star_fft = np.fft.fftn(star)
+    psf_fft = np.fft.fftn(psf)
+    return np.fft.ifftn(star_fft/psf_fft)
+
+
+focus_test = convolve2(image,new_kern)
+plt.imshow(np.real(focus_test), cmap = 'gray')
+plt.show()
+
+focus_testd = np.real(deconvolve2(focus_test,new_kern))
+plt.imshow(focus_testd,cmap = 'gray')
+plt.show()
+
+
+# show what our image looks like when it's deconvolved with the wrong
+original2  = np.real(deconvolve(deacon, motion_kernel))
+plt.imshow(original2, cmap ='gray')
+plt.show()
+
+# add some noise to the image
+n = 400
+noisy_image = test_conv.copy()
+
+noise_vec = [(np.random.randint(0,image.shape[0]), np.random.randint(0,image.shape[1])) for k in xrange(image.shape[0] * image.shape[1] / n) ]
+
+for noise in noise_vec:
+	if np.random.random() < .5:
+		noisy_image[noise] = 200
+	else:
+		noisy_image[noise] = 255
+
+
+plt.imshow(noisy_image, cmap='gray')
+plt.show()
+
+
+original2  = np.real(deconvolve(noisy_image, motion_kernel))
+plt.imshow(original2, cmap ='gray')
+plt.show()
+
+
+
+"""
+plt.show(deconv,cmap='gray')
+plt.show()
+
+dif = test2_conv - test2_conv
+print dif.shape
+zeros = np.zeros_like(dif)
+print np.allclose(dif,zeros)
+
+plt.imshow(dif, cmap = 'gray')
+plt.show()
+
+
+restored = deconvolve(test_conv,kernel)
+plt.imshow(restored, cmap = 'gray')
+plt.show()
+""" 
